f26952d75bed89da8e2d91a0cef320686f083385
Dzmitry Samuilik <dzmitry.samuilik@thomsonreuters.com>
2019-04-22 10:14:50 +0300

    Task 08: Install Redux DevTools Extension

--- src/app/core/+store/core-store.module.ts
+++ src/app/core/+store/core-store.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 // @Ngrx
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from './../../../environments/environment';
 
 @NgModule({
   imports: [
     CommonModule,
-    StoreModule.forRoot({})
+    StoreModule.forRoot({}),
+
+    // Instrumentation must be imported after importing StoreModule
+    // (config is optional)
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   declarations: []
 })
